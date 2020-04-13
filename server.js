96c38b2b027b36378fccf6acd29573a0ac24b73f
pangzong <v_zongpang@tencent.com>
2020-04-14 00:27:43 +0800

    fix server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use(express.static('/build'))
+app.use('/static', express.static('build'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
