0a83f4e61920958ef39749d77034847dda9896c9
pangzong <v_zongpang@tencent.com>
2020-04-14 06:38:38 +0800

    fix server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use('/static', express.static('build'))
+app.use('/', express.static('build'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => () => {
+//     console.log(__dirname)
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
