a5d2133aa402439b5680e005ff34df23f01c2118
parsonz <18001171283@163.com>
2020-04-21 17:34:30 +0800

    remove server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 80
-const path = require('path')
-
-app.use('/', express.static('build'))
-console.log(path.resolve(__dirname, './src/'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
