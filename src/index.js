c533954a2880b1a0f931a3d16f28580e5b1c08ec
pangzong <v_zongpang@tencent.com>
2020-04-27 18:30:31 +0800

    remove test hooks file

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-alert('test hooks')
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
